ea294ab357df8175e9479f7da368d5cafdbd3911
Valeria Udalova <udalovaval@gmail.com>
2025-10-17 20:20:46 +0300

    task 3

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ if (age < 18) {
 let age2 = Numder (prompt("Введите свой возрат."));
 const result = (age2 < 18) ? "Доступ запрещен!" : "Доступ разрешен!";
     console.log(result);
+
+    // Задание 3. swtch
+    const dayNumber = Number(prompt("Введите номер дня недели (от 1 до 7)"));
+    switch (dayNumber) {
+        case 1:
+            console.log("Понедельник");
+            break;
+        case 2:
+            console.log("Вторник");
+            break;
+        case 3:
+            console.log("Среда");
+            break;
+        case 4:
+            console.log("Четверг");
+            break;
+        case 5:
+            console.log("Пятница");
+            break;  
+        case 6:
+            console.log("Суббота");
+            break;
+        case 7:
+            console.log("Воскресенье");
+            break; 
+        default:
+            console.log("Ошибка: такого дня нет!");             
+        
+}
